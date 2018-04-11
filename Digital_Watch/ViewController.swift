b3c8f54343e0b67cd25d390cb1fee1d20cdb4f72
youngju <asdadssad@naver.coim>
2018-04-11 12:06:59 +0900

    add timer function

--- Digital_Watch/ViewController.swift
+++ Digital_Watch/ViewController.swift
@@ -9,17 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var lblTimer: UILabel!
+    var myTimer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        myTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    @objc func updateTime() {
+        let date = Date()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm:ss a"
+        lblTimer.text = formatter.string(from: date)
+        //        lblTime.text = String(describing: date)
+    }
 }
 
